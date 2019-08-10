50d4cecd91f6b861c6157862d2449f344fec9e22
Jason Hawkins <42746794+16jhawkins@users.noreply.github.com>
2019-08-10 17:44:51 -0600

    Main Window 4.0
    Adds alot of functionality to the main window

--- ShoeStore/ShoeStore/wndMain/clsMainLogic.cs
+++ ShoeStore/ShoeStore/wndMain/clsMainLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.Data;
 using System.Threading.Tasks;
 using ShoeStore.Search;
 
@@ -11,10 +12,8 @@ namespace mainWindow
     class clsMainLogic
     {
         //declare a list of all the items in the database for the data grid
-        List<clsLineItems> MyList;
 
-
-
+        ShoeStore.clsDataAccess db;
 
         clsMainSQL Query;
 
@@ -28,14 +27,18 @@ namespace mainWindow
         public clsMainLogic()
         {
             Query = new clsMainSQL();
-            MAXINVOICES = (Query.GetCountOfInvoices() + 1);
+            db = new ShoeStore.clsDataAccess();
+
+            //set the max invoices equal to the invoice count + 1
+            Int32.TryParse(db.ExecuteScalarSQL(Query.GetCountOfInvoices()), out int x);
+            MAXINVOICES = x + 1;
         }
 
         /// <summary>
         /// gets all the invoices()
         /// </summary>
         /// <returns></returns>
-        public List<clsLineItems> listItems()
+        public List<clsLineItems> ListItems()
         {
 
             try
@@ -50,17 +53,55 @@ namespace mainWindow
             }
         }
 
+
+        public List<clsLineItems> GetLineItems(int id)
+        {
+            List<clsLineItems> lstItems = new List<clsLineItems>();
+
+            int rows = 0;
+
+            string sSQL = Query.getLineItems(id);
+
+            DataSet rawdata = db.ExecuteSQLStatement(sSQL, ref rows);
+
+
+            for (int x = 0; x < rows; x++)
+            {
+                Double.TryParse(rawdata.Tables[0].Rows[x][2].ToString(), out double total);
+                Int32.TryParse(rawdata.Tables[0].Rows[x][3].ToString(), out int num);
+                lstItems.Add(new clsLineItems
+                {
+                    ItemCode = rawdata.Tables[0].Rows[x][0].ToString(),
+                    ItemDesc = rawdata.Tables[0].Rows[x][1].ToString(),
+                    Cost = total,
+                    LineItemNum = num
+
+                });
+
+            }
+
+            return lstItems;
+        }
+
+
+
         /// <summary>
         /// creates a new invoice
         /// </summary>
         /// <param name="date"></param>
         /// <param name="Line"></param>
-        public void CreateInvoice(string date)
+        public int CreateInvoice(string date, string cost, List<clsLineItems> lstItems)
         {
 
             try
             {
-                Query.CreateInvoice(date);
+                db.ExecuteNonQuery(Query.CreateInvoice(date, cost));
+
+
+                Int32.TryParse(db.ExecuteScalarSQL( Query.GetInsertedInvoiceId(date, cost)), out int id);
+                InsertLineItems(id, lstItems);
+                return id;
+
             }
             catch (Exception ex)
             {
@@ -76,11 +117,13 @@ namespace mainWindow
         /// deletes the invoice with the id equal to the selected invoice
         /// </summary>
         /// <param name="ID"></param>
-        public void Delete(int ID)
+        public void DeleteInvoice(int ID)
         {
             try
             {
-                Query.DeleteInvoice(ID);
+                DeleteLineItems(ID);
+
+                db.ExecuteNonQuery(Query.DeleteInvoice(ID));
             }
             catch (Exception ex)
             {
@@ -89,15 +132,11 @@ namespace mainWindow
             }
         }
 
-        /// <summary>
-        /// updates the specified invoice
-        /// </summary>
-        /// <param name="UpdatedInvoice"></param>
-        public void Update(clsInvoice update)
+        public void DeleteLineItems(int id)
         {
             try
             {
-                Query.UpdateInvoice(update);
+                db.ExecuteNonQuery(Query.DeleteLineItems(id));
             }
             catch (Exception ex)
             {
@@ -105,6 +144,84 @@ namespace mainWindow
                     MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
             }
         }
+
+
+        /// <summary>
+        /// updates the specified invoice
+        /// </summary>
+        /// <param name="UpdatedInvoice"></param>
+        public void UpdateInvoice(int id, string date, string cost)
+        {
+            try
+            {
+                db.ExecuteNonQuery(Query.UpdateInvoice(id, date, cost));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." +
+                    MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// updates the line items of a selected record
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="lstItems"></param>
+        public void UpdateLineItems(int id,List<clsLineItems> lstItems)
+        { 
+            try
+            {
+                //deletes all line items with id = 
+                DeleteLineItems(id);
+
+                //adds all items
+                foreach (var item in lstItems)
+                {
+                    int x = item.LineItemNum;
+                    string y = item.ItemCode;
+                    db.ExecuteNonQuery(Query.InsertLineItems(id, item));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." +
+                    MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+
+
+        }
+
+        /// <summary>
+        /// adds all line items in a new invoice
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="lstItems"></param>
+        public void InsertLineItems(int id, List<clsLineItems> lstItems)
+        {
+            try
+            {
+
+                //adds all items
+                foreach (var item in lstItems)
+                {
+                    int x = item.LineItemNum;
+                    string y = item.ItemCode;
+                    db.ExecuteNonQuery(Query.InsertLineItems(id, item));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType + "." +
+                    MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
+
+
+        }
+
 
         /// <summary>
         /// determines what if we have entered the maximum amount of invoice entries
@@ -114,7 +231,7 @@ namespace mainWindow
         {
             try
             {
-                int countOfInvoices = Query.GetCountOfInvoices();
+                Int32.TryParse(db.ExecuteScalarSQL(Query.GetCountOfInvoices()), out int countOfInvoices);
 
                 if (countOfInvoices == MAXINVOICES)
                 {
